d956873c8c7ab8a40ff09a7abd3d0f6fb56c7eca
otupa <tupa.sales@gmail.com>
2023-07-13 19:41:06 -0300

    Estrutura inicial da api.

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,7 +1,2 @@
 """Dander Init"""
 
-from .client import Client
-from .drivers import Motorists
-from .revenues import RevenuesPorcents
-from .runs import runs_factory
-from .user import User
--- src/database/querys/__init__.py
+++ src/database/querys/__init__.py
@@ -1,7 +1,2 @@
 """Sql Operatison with Sql Alchemy"""
 
-from .clients import ClientQuerys
-from .drivers import MotoristsQuerys
-from .revenues import RevenuesQuery
-from .runs import RunsQuerys
-from .users import UserQuerys
